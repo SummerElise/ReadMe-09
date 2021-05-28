9d4255280236e2e9bed247001b613f71c70c8c6e
lissy <slissy513@gmail.com>
2021-05-28 10:55:05 -0400

    added init function and revised markdown js file

--- index.js
+++ index.js
@@ -1,94 +1,105 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const util = require('util');
-const generateMarkdown = require('./generateMarkdown.js')
+const generateMarkdown = require('./generateMarkdown.js');
+generateMarkdown('testing');
 // TODO: Create an array of questions for user input
-const questions = ['What is the title of of this project?', 'Write a brief but thorough description of your project.', 'List the steps required for installing your project.', 'Provide examples of your project with guidance on how to use it.', 'Provide instruction on how developers can contribute to this project.', 'Please explain and prove examples on how to run any tests that were written for your project.', 'Choose a license for your project.', 'What is your Github username?', 'What is your email address?'];
-
-
-
-const promptUser = () => {
-    return inquirer.prompt([
+const questions = [
     {
         type: 'input',
-        message: questions[0],
+        message: 'What is the title of of this project?',
         name: 'Title',
     },
     {
         type: 'input',
-        message: questions[1],
+        message: 'Write a brief but thorough description of your project.',
         name: 'Description',
     },
     {
         type: 'input',
-        message: questions[2],
+        message: 'List the steps required for installing your project.',
         name: 'Installation',
     },
     {
         type: 'input',
-        message: questions[3],
+        message: 'Provide examples of your project with guidance on how to use it.',
         name: 'UsageInfo',
     },
     {
         type: 'input',
-        message: questions[4],
+        message: 'Provide instruction on how developers can contribute to this project.',
         name: 'Contribution',
     },
     {
         type: 'input',
-        message: questions[5],
+        message: 'Please explain and prove examples on how to run any tests that were written for your project.',
         name: 'Test',
     },
     {
         type: 'input',
-        message: questions[6],
+        message: 'Choose a license for your project.',
         name: 'License',
     },
     {
         type: 'input',
-        message: questions[7],
+        message: 'What is your Github username?',
         name: 'Github',
     },
     {
         type: 'input',
-        message: questions[8],
+        message:  'What is your email address?',
         name: 'Email',
     },
-])
-.then((answers) => {
-    console.log(answers)
-})
-}
-   
+];
  
-    fs.writeFile('README.MD',
+function writeToFile(fileName, data) {
+
+const { Title, Description, Installation, UsageInfo, Contribution, Test, License, Github, Email } = data
 
 `    
-
 # Title 
-${answers.title}
+${Title}
+
+![${License}](https://img.shields.io/badge/license-${License}-blue)
+
 ## Description
-${answers.Description}
+${Description}
+ ## Table of Contents
+
+ *[Installation](##Installation)
+ *[UsageInfo](##UsageInfo)
+ *[Contribution](##Contribution)
+ *[Test](##Tests)
+ *[Questions](##Questions)
+
 ### Installation
-${answers.Installation}
+${Installation}
 #### UsageInfo
-${answers.UsageInfo}
+${UsageInfo}
 ##### Contribution guidelines
-${answers.Contribution}
+${Contribution}
 ###### Tests
-${answers.Test}
+${Test}
 ####### License
-${answers.license}
-######## Github username
-${answers.Github}
-######### Email
-${answers.Email}
-`
-    )
+This project is licensed under the ${License} license.
+####### Questions
+If you have any questions, you can contactly me directly at ${Email}. View my other works at [${Github}](https://github.com${gitUsername})
+`    
 
-function writeToFile(fileName, data) {}
+    fs.writeFile(fileName, data, function(error) {       
+        console.log('data: ', data)
+        if(error) {
+            return console.log(error)
+        }
+    })
+}
 
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then((answers) => {
+        console.log(answers)
+        writeToFile("README.md", answers)     
+    });
+}
 
 init();
