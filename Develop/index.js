c564b64058e50ba71c2e4f53502590811ca241dd
lissy <slissy513@gmail.com>
2021-05-28 09:24:26 -0400

    added packages and created array of questions

---
+++ Develop/index.js
@@ -0,0 +1,20 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// TODO: Create an array of questions for user input
+const questions = ['What is the title of of this project?', 'Write a brief but thorough description of your project.', 'List the steps required for installing your project.', 'Provide examples of your project with guidance on how to use it.', 'Provide instruction on how developers can contribute to this project.', 'Please explain and prove examples on how to run any tests that were written for your project.', 'Choose a license for your project.', 'What is your Github username?', 'What is your email address?'];
+
+inquirer
+.prompt([
+
+])
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
